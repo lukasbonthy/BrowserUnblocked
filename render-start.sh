749ee6442ac05bf226c7c21a860c848c0e5f0f07
lukasbonthy <126826970+lukasbonthy@users.noreply.github.com>
2026-05-23 00:53:17 -0400

    Start nginx before Kasm default startup

--- render-start.sh
+++ render-start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
+
 nginx -c /etc/nginx/nginx.conf
-exec /dockerstartup/kasm_default_profile.sh
+exec /dockerstartup/kasm_default_profile.sh /dockerstartup/kasm_startup.sh --wait
